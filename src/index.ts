7c411b4c970abb0735f7084bd8d79bdb39090c57
winstxnhdw <winstxnhdw@gmail.com>
2023-09-12 03:01:44 +0800

    refactor: avoid global state

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,32 @@ import { atomic_proxy, default_proxy, resilient_proxy } from '@/proxies'
 import { OpenAPIRouter } from '@cloudflare/itty-router-openapi'
 import { createCors } from 'itty-router'
 
-const router = OpenAPIRouter({
-  docs_url: '/docs',
-  schema: {
-    info: {
-      title: 'worker-proxy',
-      description: 'A proxy for making requests to endpoint(s).',
-      version: 'v1.0.0'
+async function main(request: Request) {
+  const router = OpenAPIRouter({
+    docs_url: '/docs',
+    schema: {
+      info: {
+        title: 'worker-proxy',
+        description: 'A proxy for making requests to endpoint(s).',
+        version: 'v1.0.0'
+      }
     }
-  }
-})
+  })
 
-const { preflight, corsify } = createCors({
-  methods: ['POST'],
-  origins: ['*']
-})
+  const { preflight, corsify } = createCors({
+    methods: ['POST'],
+    origins: ['*']
+  })
 
-router.all('*', preflight)
-router.post('/', default_proxy)
-router.post('/resilient', resilient_proxy)
-router.post('/atomic', atomic_proxy)
-router.all('*', () => new Response('Not found!', { status: 404 }))
+  router.all('*', preflight)
+  router.post('/', default_proxy)
+  router.post('/resilient', resilient_proxy)
+  router.post('/atomic', atomic_proxy)
+  router.all('*', () => new Response('Not found!', { status: 404 }))
+
+  return router.handle(request).catch(console.error).then(corsify)
+}
 
 export default {
-  fetch: async (request: Request, env: unknown, ctx: unknown) =>
-    router.handle(request, env, ctx).catch(console.error).then(corsify)
+  fetch: main
 }
