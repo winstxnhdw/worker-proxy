a575f34d995d6ebf1994a2f1437edb49e957699e
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-04-25 05:19:47 +0800

    style: add braces

--- src/middlewares/json.ts
+++ src/middlewares/json.ts
@@ -4,7 +4,10 @@ export const json =
   (options = { space: 4 }): MiddlewareHandler =>
   async (context, next) => {
     await next()
-    if (context.req.query('json') !== '') return
+    
+    if (context.req.query('json') !== '') {
+      return
+    }
 
     const response = await context.res.clone().json()
     context.res = new Response(JSON.stringify(response, null, options.space), context.res)
