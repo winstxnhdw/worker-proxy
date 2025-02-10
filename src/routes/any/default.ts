0186975ff829fd9f3698e26accb531f406664795
winstxnhdw <winston.ho@redbulltechnology.com>
2025-02-10 15:06:46 +0000

    fix/ci: create passthrough proxy for every HTTP method

--- src/routes/any/default.ts
+++ src/routes/any/default.ts
@@ -1,5 +1,9 @@
 import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi'
 
+const DefaultProxyParamsSchema = z.object({
+  endpoint: z.string().openapi({ example: 'https://account.battleon.com/charpage/details?id=53251829' }),
+})
+
 const DefaultProxyBodySchema = z.object({
   body: z
     .record(z.unknown())
@@ -16,35 +20,49 @@ const DefaultProxyMultiPartSchema = z.object({
     }),
 })
 
-const route = createRoute({
-  method: 'post',
-  path: '/any/default/{endpoint}',
-  request: {
-    params: z.object({ endpoint: z.string() }),
-    body: {
-      content: {
-        'application/json': { schema: DefaultProxyBodySchema },
-        'multipart/form-data': { schema: DefaultProxyMultiPartSchema },
+const create_proxies = () => {
+  const methods = ['get', 'post', 'put', 'delete', 'patch', 'options', 'head'] as const
+  const proxy = new OpenAPIHono()
+
+  const routes = methods.map((method) =>
+    createRoute({
+      method: method,
+      path: '/any/default/{endpoint}',
+      request: {
+        params: DefaultProxyParamsSchema,
+        body: {
+          content: {
+            'text/plain': { schema: z.string().openapi({ example: '' }) },
+            'application/json': { schema: DefaultProxyBodySchema },
+            'multipart/form-data': { schema: DefaultProxyMultiPartSchema },
+          },
+        },
       },
-    },
-  },
-  responses: {
-    200: {
-      description: 'The response from the endpoint.',
-    },
-  },
-})
+      responses: {
+        200: {
+          description: 'The response from the endpoint.',
+        },
+      },
+    }),
+  )
 
-export const default_proxy = new OpenAPIHono().openapi(route, (context) => {
-  const request = context.req.raw
+  for (const route of routes) {
+    proxy.openapi(route, async (context) => {
+      const request = context.req.raw
 
-  return fetch(context.req.param('endpoint'), {
-    method: request.method,
-    headers: request.headers,
-    body: request.body,
-    redirect: request.redirect,
-    fetcher: request.fetcher,
-    integrity: request.integrity,
-    signal: request.signal,
-  })
-})
+      return fetch(context.req.param('endpoint'), {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+        redirect: request.redirect,
+        fetcher: request.fetcher,
+        integrity: request.integrity,
+        signal: request.signal,
+      })
+    })
+  }
+
+  return proxy
+}
+
+export const default_proxy = create_proxies()
